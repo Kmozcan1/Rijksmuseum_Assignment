2ccf4f982a0749ebf48ac2cef1456c05b8907b06
Kmozcan1 <kadir.ozcan@trilogy.com>
2025-01-21 13:19:48 +0100

    add additional dimensions add page size constant add error and placeholder icon resources

--- app/src/main/java/com/albertheijn/rijksmuseumassignment/presentation/theme/Dimens.kt
+++ app/src/main/java/com/albertheijn/rijksmuseumassignment/presentation/theme/Dimens.kt
@@ -3,11 +3,17 @@ package com.albertheijn.rijksmuseumassignment.presentation.theme
 import androidx.compose.ui.unit.dp
 
 object Dimens {
-    val circularLoadingIndicatorSize = 32.dp
+    val maxImageWidth = 400.dp
+
+    val standardRowHeight = 48.dp
+
+    val standardImageSize = 40.dp
 
     val standardPadding = 16.dp
 
     val standardHalfPadding = 8.dp
+
+    val standardQuarterPadding = 4.dp
 
     val topBarImageHeight = 40.dp
 }
